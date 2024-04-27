0449bd4aaf45a026489bc11793494a540891146d
bhanuteja <bhanutejasiripuram@kgpian.iitkgp.ac.in>
2024-04-27 17:45:58 +0530

    fixed the database query in read_data

--- lb/routers/read_server.py
+++ lb/routers/read_server.py
@@ -11,16 +11,16 @@ router = APIRouter()
 
 # reader - <app.server_list,metaDB>
 @app.get("/read/{server_id}")
-def read_data(server_id):
+def read_data(server_id:str):
     try:
         acquire_read()
         mysql_conn, mysql_cursor = get_db()
 
         print(f"Thread id: {threading.get_ident()}")
-        get_server_shards_query = "SELECT ShardT.Stud_id_low, ShardT.Shard_id,ShardT.Shard_size from MapT where ShardT.Shard_id=MapT.Shard_id AND MapT.Server_id=%s"
-        mysql_cursor.execute(get_server_shards_query, server_id)
+        get_server_shards_query = "SELECT S.Stud_id_low, S.Shard_id, S.Shard_size FROM ShardT S, MapT M WHERE S.Shard_id = M.Shard_id AND M.Server_id = %s"
+        mysql_cursor.execute(get_server_shards_query, (server_id,))
         resp_ = mysql_cursor.fetchall()
-        result = []
+        result = {}
         for row in resp_:
             url = f"http://{server_id}:{8000}/read"
             data = {
@@ -42,7 +42,7 @@ def read_data(server_id):
 
             print('the fethced data from :',row[1],'is ',resp)
             if resp["status"] == "success":
-                result [row[1]]= resp["data"]
+                result[row[1]] = [{"Stud_id": item[0], "Stud_name": item[1], "Stud_marks": item[2]} for item in resp["data"]]
             else:
                 return JSONResponse(
                     status_code=400,
@@ -50,18 +50,25 @@ def read_data(server_id):
                 )
         print(
             "shards_queried:",
-            shards_queried,
+            [row[1] for row in resp_],
             "data:",
             result,
         )
         return {
-            "shards_queried": shards_queried,
+            "shards_queried": [row[1] for row in resp_],
             "data": result,
             "status": "success"
         }
 
     except Exception as e:
+        error_type = type(e).__name__
+        error_message = str(e)
+        
+        print("An error occurred:")
+        print(f"Error Type: {error_type}")
+        print(f"Error Message: {error_message}")
         return "Some error"
+    
     finally:
         close_db(mysql_conn, mysql_cursor)
         release_read()
